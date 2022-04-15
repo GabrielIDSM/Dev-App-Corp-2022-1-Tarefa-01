645019b4b6b0c8c7b3a89f8ddb3c741046a85e0e
gabriel <gabrielinacio@id.uff.br>
2022-04-14 23:02:15 -0300

    home.css modified

--- src/main/java/hello/HelloServlet.java
+++ src/main/java/hello/HelloServlet.java
@@ -17,7 +17,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author viter
  */
-@WebServlet("/alomundo")
+@WebServlet("/helloworld")
 public class HelloServlet extends HttpServlet {
 
     /**
